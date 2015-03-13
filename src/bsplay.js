75e1fe9776c0ddd364d3cffb952585cb6bb2394b
Shefla <sheflaprod@free.fr>
2015-03-13 22:48:25 +0100

    Fixed player settings

--- src/bsplay.js
+++ src/bsplay.js
@@ -4,22 +4,26 @@ var plugin   = 'bsplay';
 var defaults = {};
 var settings = {
 	css: 'INJECT.css'
-, createPlayer: { markup: 'INJECT.html' }
-, playPauseClass:    'bsp-play-pause'
-, scrubberClass:     'bsp-scrubber'
-, progressClass:     'bsp-progress'
-, loaderClass:       'bsp-loaded'
-, timeClass:         'bsp-infos'
-, durationClass:     'bsp-duration'
-, playedClass:       'bsp-played'
-, errorMessageClass: 'bsp-error-message'
-, playingClass:      'bsp-playing'
-, loadingClass:      'bsp-loading'
-, errorClass:        'bsp-error'
+, createPlayer: {
+		markup: 'INJECT.html'
+	, playPauseClass:    'bsp-play-pause'
+	, scrubberClass:     'bsp-scrubber'
+	, progressClass:     'bsp-progress'
+	, loaderClass:       'bsp-loaded'
+	, timeClass:         'bsp-infos'
+	, durationClass:     'bsp-duration'
+	, playedClass:       'bsp-played'
+	, errorMessageClass: 'bsp-error-message'
+	, playingClass:      'bsp-playing'
+	, loadingClass:      'bsp-loading'
+	, errorClass:        'bsp-error'
+	}
 };
 var bsplay = {
 	init: function ($audio, options){
-		this.player = ajs.create($audio[0], settings);
+		this.options = $.extend({}, defaults, options);
+		this.player  = ajs.create($audio[0], settings);
+		console.log('init', this);
 	}
 , add: function ($audio){}
 };
