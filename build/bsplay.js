b56a3238189b6fdc4fc4725b136cc9df2c9cbad7
Shefla <sheflaprod@free.fr>
2015-03-13 22:59:48 +0100

    Added volume logic using bootstrap-slider

--- build/bsplay.js
+++ build/bsplay.js
@@ -21,9 +21,30 @@ var settings = {
 };
 var bsplay = {
 	init: function ($audio, options){
-		this.options = $.extend({}, defaults, options);
-		this.player  = ajs.create($audio[0], settings);
-		console.log('init', this);
+		var self     = this;
+		self.options = $.extend({}, defaults, options);
+		self.player  = ajs.create($audio[0], settings);
+		self.wrapper = $(this.player.wrapper);
+		self.slider  = this.wrapper.find('.bsp-volume');
+		self.slider.find('input').slider({
+			min: 0, max: 100, value: 50
+		, formatter: function (val){ return 'Volume: '+val+'%'; }
+		}).on('change', function (event){
+			var val = event.value.newValue;
+			self.player.setVolume(val / 100);
+			if (val === 0){
+				self.slider.addClass('bsp-volume-mute')
+					.removeClass('bsp-volume-half bsp-volume-full');	
+			}
+			else if (val > 50){
+				self.slider.addClass('bsp-volume-full')
+					.removeClass('bsp-volume-mute bsp-volume-half');
+			}
+			else {
+				self.slider.addClass('bsp-volume-half')
+					.removeClass('bsp-volume-mute bsp-volume-full');
+			}
+		});
 	}
 , add: function ($audio){}
 };
