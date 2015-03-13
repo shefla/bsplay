b11f45ea13d19a38d25fa9755a6b33fee9d2a34b
Shefla <sheflaprod@free.fr>
2015-03-13 20:05:10 +0100

    Added gulp task build:markup

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
-var gulp = require('gulp');
-var exec = require('child_process').exec;
-var less = require('gulp-less');
+var gulp    = require('gulp');
+var exec    = require('child_process').exec;
+var less    = require('gulp-less');
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task('build:jquery', function (done){
 	exec('cd jquery && npm run build', done);
 });
 
-gulp.task('build:style', function (done){
-	gulp.src('./src/bsplay.less')
+gulp.task('build:style', function (){
+	return gulp.src('./src/bsplay.less')
 		.pipe(less())
-		.pipe(gulp.dest('build'))
+		.pipe(gulp.dest('./build'))
+	;
+});
+
+gulp.task('build:markup', function (){
+	return gulp.src('./src/bsplay.html')
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest('./build'))
 	;
 });
