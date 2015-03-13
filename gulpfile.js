75745984bcfe162cf52a65f5a4da7886c96bb978
Shefla <sheflaprod@free.fr>
2015-03-13 19:23:06 +0100

    Added gulp task build:style

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var less = require('gulp-less');
 
 gulp.task('build:jquery', function (done){
 	exec('cd jquery && npm run build', done);
+});
+
+gulp.task('build:style', function (done){
+	gulp.src('./src/bsplay.less')
+		.pipe(less())
+		.pipe(gulp.dest('build'))
+	;
 });
